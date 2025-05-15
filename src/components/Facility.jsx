9b8b1807494c4fb19b0d04e4088d27cadfd168de
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-05-15 21:05:41 +0600

    code formating and  icon fixing

--- src/components/Facility.jsx
+++ src/components/Facility.jsx
@@ -1,50 +1,65 @@
-import HeadPhone from "./icons/HeadPhone"
-import Secure from "./icons/Secure"
-import Shipping from "./icons/Shipping"
-import Transparent from "./icons/Transparent"
-import Container from "./layouts/Container"
+import HeadPhone from "./icons/HeadPhone";
+import Secure from "./icons/Secure";
+import Shipping from "./icons/Shipping";
+import Transparent from "./icons/Transparent";
+import Container from "./layouts/Container";
 
 const Facility = () => {
   return (
     <>
-        <Container>
-            <div className=" flex justify-between items-center mb-[80px]">
-                <div className=" flex items-center gap-x-6">
-                    <HeadPhone/>
-                    <div>
-                        <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">Responsive</h3>
-                        <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">Customer service available 24/7</p>
-                    </div>
-                </div>
-                
-                <div className=" flex items-center gap-x-6">
-                    <Secure/>
-                    <div>
-                        <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">Secure</h3>
-                        <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">Certified marketplace since 2017</p>
-                    </div>
-                </div>
-                
-                <div className=" flex items-center gap-x-6">
-                    <Shipping/>
-                    <div>
-                        <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">Shipping</h3>
-                        <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">Free, fast, and reliable worldwide</p>
-                    </div>
-                </div>
-                
-                <div className=" flex items-center gap-x-6">
-                    <Transparent/>
-                    <div>
-                        <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">Transparent</h3>
-                        <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">Hassle-free return policy</p>
-                    </div>
-                </div>
-                
+      <Container>
+        <div className=" flex justify-between items-center mb-[80px]">
+          <div className=" flex items-center gap-x-6">
+            <HeadPhone />
+            <div>
+              <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">
+                Responsive
+              </h3>
+              <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">
+                Customer service available 24/7
+              </p>
             </div>
-        </Container>
-    </>
-  )
-}
+          </div>
 
-export default Facility
+          <div className=" flex items-center gap-x-6">
+            <Secure />
+            <div>
+              <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">
+                Secure
+              </h3>
+              <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">
+                Certified marketplace since 2017
+              </p>
+            </div>
+          </div>
+
+          <div className=" flex items-center gap-x-6">
+            <Shipping />
+            <div>
+              <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">
+                Shipping
+              </h3>
+              <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">
+                Free, fast, and reliable worldwide
+              </p>
+            </div>
+          </div>
+
+          <div className=" flex items-center gap-x-6">
+            <Transparent />
+            <div>
+              <h3 className=" font-montserrat text-[#303030] text-base font-bold leading-6">
+                Transparent
+              </h3>
+              <p className=" font-montserrat text-[#303030] text-base font-normal mt-0.5 leading-6">
+                Hassle-free return policy
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Facility;
