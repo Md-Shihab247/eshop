8da5eb45c3fcb855f4fc5f044dd9545eeab1fb5c
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-05-26 23:48:43 +0600

    complete timer

--- src/components/SpringSale.jsx
+++ src/components/SpringSale.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import RightArrow from "./icons/RightArrow";
 import Button from "./Button";
+import { useEffect, useState } from "react";
 
 
 function SampleNextArrow(props) {
@@ -28,6 +29,37 @@ function SamplePrevArrow(props) {
 
 const SpringSale = () => {
 
+
+  let [timerLeft, setTimerLeft] = useState(calculateTimeLeft());
+
+  function calculateTimeLeft() {
+
+    let sellEndTime = new Date("May 31, 2025 11:59:59 PM +06").getTime();
+    let now = new Date().getTime();
+    let difference = sellEndTime - now;
+    if (difference < 0) {
+        return {days : "00" , hours: "00" ,minutes: "00", second: "00"}
+    }
+    else {
+      return {
+        days : Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours : Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        minutes : Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+        second:  Math.floor((difference % (1000 * 60)) / 1000),
+      }
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+
+      setTimerLeft(calculateTimeLeft());
+
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
     const settings = {
         dots: false,
         infinite: true,
@@ -46,16 +78,22 @@ const SpringSale = () => {
         <div className=" flex justify-between items-center">
 
           <div className=" flex items-center"> 
-             <div>
+             <div className=" w-[450px]">
                <h4 className=" font-poppins text-[#303030] text-[56px] font-bold leading-[68px]">Spring Sale</h4>
-              <div className=" mt-10 font-poppins text-4xl font-semibold leading-[46px] text-[#FF624C]">
-                hello world
+              <div className=" mt-10 font-poppins flex items-center gap-x-[30px] text-4xl font-semibold leading-[46px] text-[#FF624C]">
+                <div> {timerLeft.days < 10 ? "0" + timerLeft.days : timerLeft.days} </div>
+                <span> : </span>
+                <div> {timerLeft.hours < 10 ? "0" + timerLeft.hours : timerLeft.hours} </div>
+                <span> : </span>
+                <div> {timerLeft.minutes < 10 ? "0" + timerLeft.minutes : timerLeft.minutes} </div>
+                <span> : </span>
+                <div> {timerLeft.second} </div>
               </div>
-              <div className=" mt-2 mb-[72px] font-montserrat text-base text-[#303030] leading-6 font-normal flex items-center justify-center gap-x-[58px]">
-                <div>Days</div>
-                <div>Hours</div>
-                <div>Minutes</div>
-                <div>Seconds</div>
+              <div className=" mt-2 mb-[72px] font-montserrat text-base text-[#303030] leading-6 font-normal flex items-center">
+                <div className=" ml-[3px]">Days</div>
+                <div className=" ml-[72px]">Hours</div>
+                <div className=" ml-[52px]">Minutes</div>
+                <div className=" ml-[45px]">Seconds</div>
               </div>
               <Button content={"Shop Now"}/>
              </div>
