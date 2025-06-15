09db21ab8db990bcbc069b5a63d35a30a17d9d06
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-06-15 13:38:11 +0600

    product-list ,  left side

---
+++ src/components/icons/UncheckIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UncheckIcon = () => {
+  return ( 
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" y="0.5" width="15" height="15" rx="1.5" fill="#F4F4F4" stroke="#303030"/>
+    </svg>
+  )
+}
+
+export default UncheckIcon
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,8 +1,182 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Container from '../components/layouts/Container'
+import { IoIosArrowDown } from 'react-icons/io'
+import CheckIcon from '../components/icons/CheckIcon'
+
+  let Categories = [
+    { id: 1, name: 'Computers & Tablets' },
+    { id: 2, name: 'Mobile & Accessories' },
+    { id: 3, name: 'TV & Home Theater' },
+    { id: 4, name: 'Audio & Headphones' },
+    { id: 5, name: 'Cameras & Camcorders' },
+    { id: 6, name: 'Gaming Equipment' },
+    { id: 7, name: 'Home Appliances' },
+  ]
+
+  let Brands = [
+    { id: 1, name: 'Apple' , available : "( 565 )" },
+    { id: 2, name: 'Samsung' , available : "( 428 )" },
+    { id: 3, name: 'ASUS' , available : "( 323 )" },
+    { id: 4, name: 'Dell' , available : "( 298 )" },
+    { id: 5, name: 'Lenovo' , available : "( 180 )" },
+    { id: 6, name: 'HP' , available : "( 98 )" },
+    { id: 7, name: 'Panasonic' , available : "( 17 )" }
+  ]
 
 const ProductListPage = () => {
+
+   let [isDropDownOpen, setIsDropDownOpen] = useState(true);
+   let [isDropDownOpen2, setIsDropDownOpen2] = useState(true);
+   let [isDropDownOpen3, setIsDropDownOpen3] = useState(true);
+   let [index, setIndex] = useState([]);
+   let [index2, setIndex2] = useState([]);
+   let [minValue,setMinValue] = useState(0)
+   let [maxValue,setMaxValue] = useState(1000)
+    let handleSelect = (indexed)=>{       
+      if (index.includes(indexed)) {
+          setIndex((prev)=> prev.filter((num)=> num !== indexed))
+      }
+      else{
+        setIndex([...index, indexed])
+      }
+       
+    }
+    
+    let handleSelect2 = (indexed)=>{       
+      if (index2.includes(indexed)) {
+          setIndex2((prev)=> prev.filter((num)=> num !== indexed))
+       }
+      else{
+        setIndex2([...index2, indexed])
+       }
+       
+    }
+    
+
+    let updateSlider = (type,value)=>{
+        console.log(value);
+        
+        if (type === "min") {
+          let newMin = Math.min(parseInt(value), maxValue)
+          setMinValue(newMin)
+        }
+        else{
+          
+          let newMax = Math.max(parseInt(value), minValue)
+          setMaxValue(newMax)
+        }
+    }
+
+  let minParcet = (minValue / 1000) * 100 
+  let maxParcet = (maxValue / 1000) * 100 
+
   return (
-    <div>ProductListPage</div>
+    <div className='mt-16'>
+      <Container>
+      <div className=' flex'>
+
+        <div className='w-[355px] max-h-[1057px] box-border p-12 bg-[#F4F4F4] rounded-[25px]'>
+          <div className='w-full'> 
+             
+                                                      {/* Categories Dropdown  */} 
+              <div >
+               <div onClick={()=> setIsDropDownOpen(!isDropDownOpen)} className=' mb-5 flex items-center justify-between'>
+                  <h4 className=' cursor-pointer font-montserrat text-xl text-[#303030] font-bold  leading-7.5'>Categories</h4>
+                  <IoIosArrowDown  className={`${isDropDownOpen && `rotate-180`} cursor-pointer transition-all text-black h-4 w-4 `}/>
+               </div>
+
+               {isDropDownOpen &&        
+                <ul className={ ` overflow-hidden transition-all`}> 
+                    {Categories.map((category,indexed) => (
+                      <li onClick={()=> handleSelect(indexed)} key={category.id}  className={`${index === index && `click`}  flex cursor-pointer gap-x-2 items-center mt-3`}>
+                        <CheckIcon isChecked={index.includes(indexed)}/>
+                        <span className={ ` text-[#303030] text-base font-montserrat transition-all leading-6 ${index.includes(indexed) ? "font-bold" : "font-normal"}`}>{category.name}</span>
+                      </li>
+                    ))}
+                </ul>
+              }   
+              </div>
+
+              <hr className=' text-[#C3C3C3] w-full my-10'/>
+                                                    {/* Brands Dropdown  */}
+              <div>
+                  <div onClick={()=> setIsDropDownOpen2(!isDropDownOpen2)} className=' mb-5 flex items-center justify-between'>
+                    <h4 className=' cursor-pointer font-montserrat text-xl text-[#303030] font-bold  leading-7.5'>Brands</h4>
+                    <IoIosArrowDown   className={`${isDropDownOpen2 && `rotate-180`} cursor-pointer transition-all text-black h-4 w-4 `}/>
+                  </div>
+
+                {isDropDownOpen2 &&        
+                  <ul className={ ` overflow-hidden transition-all`}> 
+                      {Brands.map((category,indexed) => (
+                        <li onClick={()=> handleSelect2(indexed)} key={category.id}  className={`${index === index && `click`}  flex cursor-pointer justify-between items-center mt-3`}>
+                          <span className=' gap-x-2 flex items-center'>
+                          <CheckIcon isChecked={index2.includes(indexed)}/>
+                          <span className={ ` text-[#303030] text-base font-montserrat transition-all leading-6 ${index2.includes(indexed) ? "font-bold" : "font-normal"}`}>{category.name}</span>
+                          </span>
+                          <span className='text-[#303030] font-montserrat text-base font-normal leading-6'> {category.available} </span>
+                        </li>
+                      ))}
+                      <a href='#' className=' inline-block text-[#303030] text-base font-montserrat font-bold leading-6 mt-5 underline underline-offset-5'>More Brands</a>
+                  </ul>
+                }   
+              </div>
+
+              <hr className=' text-[#C3C3C3] w-full my-10'/>
+
+             <div>
+                <div onClick={()=> setIsDropDownOpen3(!isDropDownOpen3)} className=' mb-5 flex items-center justify-between'>
+                    <h4 className=' cursor-pointer font-montserrat text-xl text-[#303030] font-bold  leading-7.5'>Price</h4>
+                    <IoIosArrowDown   className={`${isDropDownOpen3 && `rotate-180`} cursor-pointer transition-all text-black h-4 w-4 `}/>
+                </div>
+
+                {isDropDownOpen3 && 
+                <div>
+                  <div className=' flex justify-between'>
+                    <div className='h-[74px] w-[124px] flex items-center justify-center font-montserrat text-base font-normal leading-6 text-[#303030] border border-[#929292] rounded-[10px] bg-[#F4F4F4]'> ${minValue} </div>
+                    <div className='h-[74px] w-[124px] flex items-center justify-center font-montserrat text-base font-normal leading-6 text-[#303030] border border-[#929292] rounded-[10px] bg-[#F4F4F4]'> ${maxValue} </div>
+                  </div>
+
+                  <div className=' mt-7.5'>
+                     <div className=' relative w-full h-0.5 bg-[#ddd] rounded'>
+                       <div 
+                       className={` absolute bg-[#FF624C] rounded h-full`}
+                       style={{left: `${minParcet}%`, width: `${maxParcet - minParcet}%`}}
+                       ></div>
+
+                       <input 
+                       type="range"
+                       min={0}
+                       max={1000}
+                       step={10}
+                       value={minValue}
+                       onChange={(e)=> updateSlider("min",e.target.value)}
+                       className='w-full h-2 bg-transparent pointer-events-none  absolute top-[-2px] appearance-none'
+                       />
+
+                       <input 
+                       type="range"
+                       min={0}
+                       max={1000}
+                       step={10}
+                       value={maxValue}
+                       onChange={(e)=> updateSlider("max",e.target.value)}
+                       className='w-full h-2 bg-transparent pointer-events-none absolute top-[-2px] appearance-none'
+                       />
+                     </div>
+                  </div>
+                </div>
+
+                }
+             </div>
+
+          </div>
+
+        </div>
+
+        <div className='w-[75%]'>Products</div>
+      </div>
+    </Container>
+    </div>
   )
 }
 
