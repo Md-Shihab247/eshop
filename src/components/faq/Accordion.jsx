7234409ef66d9fcd97b08c2d0cad47887e059f6b
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-07-27 23:04:50 +0600

    Update Accordion.jsx

--- src/components/faq/Accordion.jsx
+++ src/components/faq/Accordion.jsx
@@ -3,13 +3,13 @@ import AccordionIcon from "../icons/AccordionIcon"
 const Accordion = ({question,answer,isOpen,activeToggle}) => {
     
   return (
-    <button onClick={activeToggle}  className={` transition-all cursor-pointer duration-300 w-full flex justify-start gap-x-2 sm:gap-x-8 bg-white rounded-[15px] sm:rounded-[25px] py-4 sm:py-[33px] px-2 sm:pl-12 sm:pr-[56px] mb-3 sm:mb-6 last:mb-0`} >
+    <button onClick={activeToggle}  className={` transition-all cursor-pointer duration-300 w-full flex justify-start gap-x-2 sm:gap-x-8 bg-white rounded-[15px] sm:rounded-[25px] py-4 sm:py-[33px] px-3 sm:pl-12 sm:pr-[56px] mb-3 sm:mb-6 last:mb-0`} >
         <div className=" mt-[3px] sm:mt-0">
         <AccordionIcon active= {isOpen}  toggle={activeToggle}/>
         </div>
 
         <div>
-           <h3 className=" text-[#303030] text-start font-poppins text-[15px] sm:text-2xl font-semibold leading-7.5"> {question.question} </h3>
+           <h3 className=" text-[#303030] text-start font-poppins text-sm sm:text-2xl font-semibold leading-7.5"> {question.question} </h3>
 
            {isOpen 
            &&
