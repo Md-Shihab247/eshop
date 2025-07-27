1aefe63821fb817438be18e295ee54fc64ed174f
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-07-27 22:50:19 +0600

    Update Accordion.jsx

--- src/components/faq/Accordion.jsx
+++ src/components/faq/Accordion.jsx
@@ -3,7 +3,7 @@ import AccordionIcon from "../icons/AccordionIcon"
 const Accordion = ({question,answer,isOpen,activeToggle}) => {
     
   return (
-    <button onClick={activeToggle}  className={` transition-all cursor-pointer duration-300 w-full flex justify-start gap-x-1 sm:gap-x-8 bg-white rounded-[15px] sm:rounded-[25px] py-4 sm:py-[33px] px-2 sm:pl-12 sm:pr-[56px] mb-3 sm:mb-6 last:mb-0`} >
+    <button onClick={activeToggle}  className={` transition-all cursor-pointer duration-300 w-full flex justify-start gap-x-2 sm:gap-x-8 bg-white rounded-[15px] sm:rounded-[25px] py-4 sm:py-[33px] px-2 sm:pl-12 sm:pr-[56px] mb-3 sm:mb-6 last:mb-0`} >
         <div className=" mt-[3px] sm:mt-0">
         <AccordionIcon active= {isOpen}  toggle={activeToggle}/>
         </div>
